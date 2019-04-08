b2105f1c78d6c73ab8fb139deb6ad22edaf35cf0
aerlin17 <aerlin17@student.aau.dk>
2019-04-08 10:34:49 +0200

    GrammarBuilder tests done

--- test/test_grammarBuilder.py
+++ test/test_grammarBuilder.py
@@ -1,7 +1,7 @@
 import os
 from unittest import TestCase
 
-from src.grammar import GrammarBuilder, Rule, Production, Nonterminal, Terminal, Symbol, LAMBDA, Grammar
+from src.grammar import GrammarBuilder, Rule, Production, Nonterminal, Terminal, LAMBDA
 
 
 class TestGrammarBuilder(TestCase):
@@ -12,41 +12,31 @@ class TestGrammarBuilder(TestCase):
         cls.grammar = GrammarBuilder.build_grammar_from_file(grammar_file)
         cls.grammar_builder = GrammarBuilder()
 
-        # Set up of manual grammar builder
-        cls.terminals = ['a', 'b', 'c', 'd', 'q', '$', LAMBDA]
-        cls.nonterminals = ['S', 'A', 'B', 'C', 'Q']
-
-        cls.grammar_builder_manual = GrammarBuilder()
-        cls.grammar_builder_manual.add_terminals(cls.terminals)
-        cls.grammar_builder_manual.add_nonterminals(cls.nonterminals)
-
-        cls.grammar_builder_manual.add_rule('S', ['A', 'C', '$'])
-        cls.grammar_builder_manual.add_rule('C', ['c'])
-        cls.grammar_builder_manual.add_rule('C', [LAMBDA])
-        cls.grammar_builder_manual.add_rule('A', ['a', 'B', 'C', 'd'])
-        cls.grammar_builder_manual.add_rule('A', ['B', 'Q'])
-        cls.grammar_builder_manual.add_rule('B', ['b', 'B'])
-        cls.grammar_builder_manual.add_rule('B', [LAMBDA])
-        cls.grammar_builder_manual.add_rule('Q', ['q'])
-        cls.grammar_builder_manual.add_rule('Q', [LAMBDA])
-
-        cls.builder_rules = cls.grammar_builder_manual.rules
-
-
-    @classmethod
-    def tearDownClass(cls):  # After all tests
-        pass
-
     def setUp(self):  # Before each test
-        pass
+        # Set up of manual grammar builder
+        self.terminals = ['a', 'b', 'c', 'd', 'q', '$', LAMBDA]
+        self.nonterminals = ['S', 'A', 'B', 'C', 'Q']
 
-    def tearDown(self):  # After each test
-        pass
+        self.grammar_builder_manual = GrammarBuilder()
+        self.grammar_builder_manual.add_terminals(self.terminals)
+        self.grammar_builder_manual.add_nonterminals(self.nonterminals)
+
+        self.grammar_builder_manual.add_rule('S', ['A', 'C', '$'])
+        self.grammar_builder_manual.add_rule('C', ['c'])
+        self.grammar_builder_manual.add_rule('C', [LAMBDA])
+        self.grammar_builder_manual.add_rule('A', ['a', 'B', 'C', 'd'])
+        self.grammar_builder_manual.add_rule('A', ['B', 'Q'])
+        self.grammar_builder_manual.add_rule('B', ['b', 'B'])
+        self.grammar_builder_manual.add_rule('B', [LAMBDA])
+        self.grammar_builder_manual.add_rule('Q', ['q'])
+        self.grammar_builder_manual.add_rule('Q', [LAMBDA])
+
+        self.builder_rules = self.grammar_builder_manual.rules
 
     #################
     #     TESTS     #
     #################
-    # ADD FUNCTIONS #
+    #   test_add_*  #
     #################
 
     def test_add_terminals(self):
@@ -91,6 +81,17 @@ class TestGrammarBuilder(TestCase):
         builder_rule = Rule(x_nonterminal, y_production)
         self.assertEqual(builder_rule.__str__(), test_rule.__str__())
 
+    def test_add_rules_from_file(self):
+        grammar_file = os.path.abspath('../src/resources/testgrammar.txt')
+        test_builder = GrammarBuilder()
+        test_builder.add_rules_from_file(grammar_file)
+
+        self.assertEqual(test_builder.rules, self.builder_rules)
+
+    ###################
+    # everything else #
+    ###################
+
     def test_build_grammar_from_file(self):
         test_grammar = self.grammar_builder_manual.build()
         self.assertEqual(self.grammar.to_str(), test_grammar.to_str())
@@ -104,40 +105,43 @@ class TestGrammarBuilder(TestCase):
                                                                                       self.nonterminals)
         self.assertEqual(set(terminals_from_rules), set(self.terminals))
 
-    def test_add_rules_from_file(self):
-        grammar_file = os.path.abspath('../src/resources/testgrammar.txt')
-        test_builder = GrammarBuilder()
-        test_builder.add_rules_from_file(grammar_file)
-
-        self.assertEqual(test_builder.rules, self.builder_rules)
-
-    def test__format_line_1(self):
-        test_string = 'S -> A, C, $'
-        test_split = self.grammar_builder._format_line(test_string, 1)
-        self.assertEqual(test_split, ('S', ['A', 'C', '$']))
-
-    def test__format_line_2(self):
-        test_string = 'C -> LAMBDA'
-        test_split = self.grammar_builder._format_line(test_string, 1)
-        self.assertEqual(test_split, ('C', ['λ']))
-
-    def test__format_line_3(self):
-        test_string = 'A -> a, B, C, d'
-        test_split = self.grammar_builder._format_line(test_string, 1)
-        self.assertEqual(test_split, ('A', ['a', 'B', 'C', 'd']))
-
-    def test__format_line_4(self):
-        test_string = 'B -> b, B'
-        test_split = self.grammar_builder._format_line(test_string, 1)
-        self.assertEqual(test_split, ('B', ['b', 'B']))
-
-    def test__format_line_5(self):
-        test_string = 'Q -> q'
-        test_split = self.grammar_builder._format_line(test_string, 1)
-        self.assertEqual(test_split, ('Q', ['q']))
+    def test__format_line(self):
+        self.assertEqual(self.grammar_builder._format_line('S -> A, C, $', 1), ('S', ['A', 'C', '$']))
+        self.assertEqual(self.grammar_builder._format_line('C -> LAMBDA', 1), ('C', ['λ']))
+        self.assertEqual(self.grammar_builder._format_line('A -> a, B, C, d', 1), ('A', ['a', 'B', 'C', 'd']))
+        self.assertEqual(self.grammar_builder._format_line('B -> b, B', 1), ('B', ['b', 'B']))
+        self.assertEqual(self.grammar_builder._format_line('Q -> q', 1), ('Q', ['q']))
 
     def test_build(self):
-        pass
+        test_grammar = self.grammar_builder_manual.build()
+        self.assertEqual(test_grammar.to_str(), self.grammar.to_str())
 
     def test__get_production(self):
-        pass
+        terminal_dict = {}
+        nonterminal_dict = {}
+
+        for terminal in self.grammar_builder_manual.terminals:
+            terminal_dict[terminal] = Terminal(terminal)
+
+        for nonterminal in self.grammar_builder_manual.nonterminals:
+            nonterminal_dict[nonterminal] = Nonterminal(nonterminal)
+
+        for rule in self.grammar.rules:
+            lhs = rule.LHS.name
+            rhs = [symbol.name for symbol in rule.RHS.symbols]
+            self.grammar_builder.add_rule(lhs, rhs)
+
+        LHS_1, RHS_1 = self.grammar_builder_manual.rules[0]
+        LHS_2, RHS_2 = self.grammar_builder_manual.rules[3]
+        LHS_3, RHS_3 = self.grammar_builder_manual.rules[6]
+        LHS_4, RHS_4 = self.grammar_builder_manual.rules[7]
+
+        test_production_1 = self.grammar_builder_manual._get_production(RHS_1, terminal_dict, nonterminal_dict)
+        test_production_2 = self.grammar_builder_manual._get_production(RHS_2, terminal_dict, nonterminal_dict)
+        test_production_3 = self.grammar_builder_manual._get_production(RHS_3, terminal_dict, nonterminal_dict)
+        test_production_4 = self.grammar_builder_manual._get_production(RHS_4, terminal_dict, nonterminal_dict)
+
+        self.assertEqual(test_production_1.__str__(), 'A C $')
+        self.assertEqual(test_production_2.__str__(), 'a B C d')
+        self.assertEqual(test_production_3.__str__(), LAMBDA)
+        self.assertNotEqual(test_production_4.__str__(), 'a')
