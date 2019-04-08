f02ea47e81f6a0d2942fcc179ae7d7522e1044f6
aerlin17 <aerlin17@student.aau.dk>
2019-04-08 13:46:54 +0200

    tests for stack class done

--- test/test_stack.py
+++ test/test_stack.py
@@ -1,4 +1,7 @@
+import os
 from unittest import TestCase
+
+from src.parser import Stack, GrammarBuilder, LAMBDA
 
 
 class TestStack(TestCase):
@@ -12,7 +15,12 @@ class TestStack(TestCase):
         pass
 
     def setUp(self):  # Before each test
-        pass
+        self.stack = Stack()
+        self.stack.push('$')
+        self.stack.push('A')
+        self.stack.push('c')
+        self.stack.push('Q')
+        self.stack.push('b')
 
     def tearDown(self):  # After each test
         pass
@@ -22,13 +30,46 @@ class TestStack(TestCase):
     #########
 
     def test_is_empty(self):
-        self.fail()
+        self.assertFalse(self.stack.is_empty())
+        self.stack.pop()
+        self.stack.pop()
+        self.stack.pop()
+        self.stack.pop()
+        self.stack.pop()
+        self.assertTrue(self.stack.is_empty())
 
     def test_push(self):
-        self.fail()
+        self.stack.push('C')
+        self.assertEqual(self.stack.top_of_stack(), 'C')
+
+        self.stack.push('A')
+        self.assertEqual(self.stack.top_of_stack(), 'A')
+
+        self.stack.push('a')
+        self.assertEqual(self.stack.top_of_stack(), 'a')
+
+        self.stack.push('Q')
+        self.assertEqual(self.stack.top_of_stack(), 'Q')
+
+        self.stack.push('q')
+        self.assertNotEqual(self.stack.top_of_stack(), 'b')
 
     def test_pop(self):
-        self.fail()
+        self.assertEqual(self.stack.pop(), 'b')
+        self.assertEqual(self.stack.pop(), 'Q')
+        self.assertEqual(self.stack.pop(), 'c')
+        self.assertEqual(self.stack.pop(), 'A')
+        self.assertEqual(self.stack.pop(), '$')
 
     def test_top_of_stack(self):
-        self.fail()
+        self.assertEqual(self.stack.top_of_stack(), 'b')
+        self.stack.pop()
+        self.assertEqual(self.stack.top_of_stack(), 'Q')
+        self.stack.pop()
+        self.assertEqual(self.stack.top_of_stack(), 'c')
+        self.stack.pop()
+        self.assertEqual(self.stack.top_of_stack(), 'A')
+        self.stack.pop()
+        self.assertEqual(self.stack.top_of_stack(), '$')
+        self.stack.push('B')
+        self.assertNotEqual(self.stack.top_of_stack(), 'A')
