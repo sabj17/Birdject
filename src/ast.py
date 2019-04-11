483c039775d63be832ce99a86bce5a1cd5e9b01e
MathiasAAU <mkra17@student.aau.dk>
2019-04-11 13:21:02 +0200

    some node stuff

--- src/ast.py
+++ src/ast.py
@@ -4,6 +4,11 @@ class AbstractNode:
         pass
 
 
+class ProgNode(AbstractNode):
+    def __init__(self, stmts):
+        self.stmts = stmts
+
+
 ###############
 # STATEMENTS #
 ###############
@@ -16,11 +21,33 @@ class IfStmtNode(StatementNode):
 
 
 class WhenStmtNode(StatementNode):
-    pass
+    def __init__(self, expression, block):
+        self.expression = expression
+        self.block = block
 
 
 class ForStmtNode(StatementNode):
+    def __init__(self, id, expression, block):
+        self.id = id
+        self.expression = expression
+        self.block = block
+
+
+class VarDcl(StatementNode):
     pass
+
+
+class FuncDcl(StatementNode):
+    def __init__(self, id, params, block):
+        self.id = id
+        self.params = params
+        self.block = block
+
+
+class ClassDcl(StatementNode):
+    def __init__(self, id, body_parts):
+        self.id = id
+        self.body_parts = body_parts
 
 
 ###############
@@ -31,70 +58,144 @@ class ExpressionNode(AbstractNode):
 
 
 class BinaryExpNode(ExpressionNode):
-    pass
+    def __init__(self, expr1, expr2):
+        self.expr1 = expr1
+        self.expr2 = expr2
+        # Maybe have operand here instead of having many classes for it?
 
 
 class UnaryExpNode(ExpressionNode):
     pass
 
 
-#####################
 # UNARY EXPRESSIONS #
-#####################
 class NotNode(UnaryExpNode):
     pass
 
 
-######################
 # BINARY EXPRESSIONS #
-######################
 class PlusNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class MinusNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class MultiplyNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class DivideNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class ModuloNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class EqualsNode(BinaryExpNode):
-    pass
-
-
-class EqualNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class NotEqualNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class GreaterThanNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class LessThanNode(BinaryExpNode):
-    pass
-
-
-class Node(BinaryExpNode):
-    pass
-
-
-class OrNode(BinaryExpNode):
-    pass
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
 
 
 class AndNode(BinaryExpNode):
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
+
+
+class OrNode(BinaryExpNode):
+    def __init__(self, expr1, expr2):
+        super().__init__(expr1, expr2)
+
+
+###############
+# BLOCK STUFF #
+###############
+class BlockNode(AbstractNode):
     pass
+
+
+class BlockBodyPartNode(AbstractNode):
+    pass
+
+
+class ReturnNode(BlockBodyPartNode):
+    def __init__(self, expression):
+        self.expression = expression
+
+
+class BreakNode(BlockBodyPartNode):
+    pass
+
+
+class RunNode(BlockBodyPartNode):
+    pass
+
+
+###############
+# CLASS STUFF #
+###############
+class ClassBodyPart(AbstractNode):
+    def __init__(self, dcl):
+        self.dcl = dcl
+
+
+###################
+# PARAMETER STUFF #
+###################
+class ParameterNode(AbstractNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+##############
+# TERM STUFF #
+##############
+class TermNode(AbstractNode):
+    pass
+
+
+class BoolNode(TermNode):
+    pass
+
+
+class StringNode(TermNode):
+    pass
+
+
+class ValueNode(TermNode):  # Maybe have float and int node instead?
+    pass
+
+
+class IdNode(TermNode):
+    pass
+
+
+'''
+Need to do
+Array
+Dot notation
+Id operation
+
+'''
