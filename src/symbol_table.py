be741d20f209f2c93912ba24042fecf0dc8b780a
aerlin17 <aerlin17@student.aau.dk>
2019-05-02 09:38:31 +0200

    Need to fix visits of NoneType, FunctionNode and WhenNode

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -12,20 +12,18 @@ class SymbolTable:
         self.process_node(ast_root)
 
     def process_node(self, node):
-        if isinstance(node, TermNode):
+        if isinstance(node, str):
             if not self.is_declared_locally(node):
-                self.add_symbol(node.__str__)
+                self.add_symbol(node)
 
         elif isinstance(node, BlockNode) or isinstance(node, ClassBodyNode):
             self.open_scope(node)
 
-        elif isinstance(node, AssignNode):
-            node_vars = vars(node)
-            for child in node_vars.values():
-                if isinstance(child, str):
-                    self.add_symbol(child.__str__())
+        elif isinstance(node, (AssignNode, ClassBodyNode)):
+            self.add_symbol(node.id.name)
 
-        self.process_all_children_nodes(node)
+        if node is not None and not isinstance(node, str):
+            self.process_all_children_nodes(node)
 
         if isinstance(node, BlockNode):
             self.close_scope()
@@ -33,30 +31,27 @@ class SymbolTable:
     def process_all_children_nodes(self, node):
         try:
             node_children = vars(node)
-            for child in node_children.values():
-                if isinstance(child, list):  # if node has more than one child, child is a list
-                    for cc in child:
-                        self.process_node(cc)
-                elif isinstance(child, str):
-                    self.add_symbol(child)
-                else:
-                    self.process_node(child)  # powerful recursjens
+            if node_children:
+                for child in node_children.values():
+                    if isinstance(child, list):  # if node has more than one child, child is a list
+                        for cc in child:
+                            self.process_node(cc)
+                    else:
+                        self.process_node(child)  # powerful recursjens
         except:
-            print("EXCEPTION", type(node))
+            print("EXCEPTION", type(node), "Children:", vars(node))  # TODO: use attributes from node classes, such as FunctionNode.id
 
     def open_scope(self, block_node):
         table_scope = SymbolTable(block_node)
         self.scope[table_scope] = None  # TODO: review
         self.scope_stack.push(table_scope)
-        print(self.current_scope)
         self.set_current_scope()
-        print(self.current_scope)
 
     def close_scope(self):
         self.scope_stack.pop()
         self.set_current_scope()
 
-    def set_current_scope(self):
+    def set_current_scope(self):    # TODO: Check if actually changes current scope
         self.current_scope = self.scope_stack.top_of_stack()
 
     def add_symbol(self, node_name):
