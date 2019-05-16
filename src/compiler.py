0ef7e776ff11905ec777f9fd85ca0c6cbd8cd2cc
MathiasAAU <mkra17@student.aau.dk>
2019-05-16 12:33:54 +0200

    added pin5

--- src/compiler.py
+++ src/compiler.py
@@ -13,7 +13,7 @@ from src.ast import GraphASTVisitor
 
 wd = os.getcwd()
 grammar_file = os.path.join(wd, 'resources/grammar.txt')
-program_file = os.path.join(wd, 'resources/Example.jnr')
+program_file = os.path.join(wd, 'resources/TestMultipleRooms.jnr')
 keyword_file = os.path.join(wd, 'resources/keywords.txt')
 token_spec_file = os.path.join(wd, 'resources/token_spec.txt')
 
@@ -53,8 +53,7 @@ ast.accept(GraphASTVisitor())
 visitor = AstNodeVisitor()
 ast.accept(visitor)
 symtable = visitor.current_scope
-print("The entire table")
-print(symtable.symbols)
+#print(symtable.symbols)
 
-#codeVisitor = Visitor(Program(), symtable)
-#codeVisitor.visit(ast.prog)
+codeVisitor = Visitor(Program(), symtable)
+codeVisitor.visit(ast.prog)
